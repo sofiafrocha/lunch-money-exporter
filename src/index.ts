20d8bd72e980927f8f6d52262aa7a3672dbb8206
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2023-06-15 21:50:54 +0100

    Start the cli part

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,16 @@ import dayjs from 'dayjs';
 import * as fs from 'fs';
 import { json2csv } from 'json-2-csv';
 
+import { intro, outro, text, confirm } from '@clack/prompts';
+
 dotenv.config();
 
 const API_URL = 'https://dev.lunchmoney.app/v1/';
 const DEFAULT_NAME = 'exported-transactions';
 
-function makeAPIRequest(url: string, params: object = {}) {
+function makeAPIRequest(url: string, params: object = {}, apiToken: string) {
   const config = {
-    headers: { Authorization: `Bearer ${process.env.API_TOKEN}` }
+    headers: { Authorization: `Bearer ${apiToken}` }
   }
   return axios.get(url, {
     params: params,
@@ -22,16 +24,21 @@ function makeAPIRequest(url: string, params: object = {}) {
 function getAccountInfo() {
   return makeRequest(API_URL + 'me');
 }
+
+function getTransactionProperties(transactions = []) {
+  const [firstTransaction] = transactions;
+  return Object.keys(firstTransaction);
+}
 */
 
-function getTransactions(offset: number = 0, limit: number = 100, startDate: string = '2020-01-01', endDate: string = dayjs().format('YYYY-MM-DD')) {
+function getTransactions(apiKey: string, offset: number = 0, limit: number = 100, startDate: string = '2020-01-01', endDate: string = dayjs().format('YYYY-MM-DD')) {
 
   return makeAPIRequest(API_URL + 'transactions', {
     start_date: startDate,
     end_date: endDate,
     ...(offset ? { offset } : {}),
     ...(limit ? { limit } : {})
-  });
+  }, apiKey);
 }
 
 function convertToCSV(input: Array<object> = [], outputFileName: string = `${DEFAULT_NAME}.csv`) {
@@ -44,10 +51,34 @@ function convertToCSV(input: Array<object> = [], outputFileName: string = `${DEF
 }
 
 (async () => {
+  /*
   const { data } = await getTransactions();
   const { transactions } = data;
 
   fs.writeFileSync(`${DEFAULT_NAME}.json`, JSON.stringify(transactions));
   convertToCSV(transactions);
+  */
+
+  intro(`create-my-app`);
+  const apiKey = await text({
+    message: 'Add your API key. You can get it on https://my.lunchmoney.app/developers!',
+    validate(value) {
+      if (value.length === 0) return `Value is required!`;
+    },
+  });
+
+  const saveJsonFile = await confirm({
+    message: 'Do you want to save it on a JSON file too?',
+  });
+
+  const { data } = await getTransactions(String(apiKey));
+  const { transactions } = data;
+
+  if (saveJsonFile) {
+    fs.writeFileSync(`${DEFAULT_NAME}.json`, JSON.stringify(transactions));
+  }
+  convertToCSV(transactions);
+
+  outro(`You're all set!`);
 })();
 
