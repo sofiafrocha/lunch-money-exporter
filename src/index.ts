a9a436235db903a4a048746aa237d629d1b85339
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2023-06-18 22:19:02 +0100

    Ask questions, do things, give feedback

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ import dayjs from 'dayjs';
 import * as fs from 'fs';
 import { json2csv } from 'json-2-csv';
 
-import { intro, outro, text, confirm } from '@clack/prompts';
+import { intro, outro, text, confirm, spinner } from '@clack/prompts';
 
 dotenv.config();
 
+const s = spinner();
 const API_URL = 'https://dev.lunchmoney.app/v1/';
 const DEFAULT_NAME = 'exported-transactions';
 
@@ -20,19 +21,14 @@ function makeAPIRequest(url: string, params: object = {}, apiToken: string) {
     ...config,
   });
 }
-/*
-function getAccountInfo() {
-  return makeRequest(API_URL + 'me');
-}
 
-function getTransactionProperties(transactions = []) {
-  const [firstTransaction] = transactions;
-  return Object.keys(firstTransaction);
-}
-*/
-
-function getTransactions(apiKey: string, offset: number = 0, limit: number = 100, startDate: string = '2020-01-01', endDate: string = dayjs().format('YYYY-MM-DD')) {
-
+function getTransactions(
+  apiKey: string,
+  startDate: string = '2020-01-01',
+  endDate: string = dayjs().format('YYYY-MM-DD'),
+  offset: number = 0,
+  limit: number = 100,
+) {
   return makeAPIRequest(API_URL + 'transactions', {
     start_date: startDate,
     end_date: endDate,
@@ -41,29 +37,49 @@ function getTransactions(apiKey: string, offset: number = 0, limit: number = 100
   }, apiKey);
 }
 
-function convertToCSV(input: Array<object> = [], outputFileName: string = `${DEFAULT_NAME}.csv`) {
+function convertToCSV(input: Array<object> = [], outputFileName: string = DEFAULT_NAME) {
   json2csv(input)
     .then((result) => {
       console.log('number of transactions saved: ', result.length);
-      fs.writeFileSync(outputFileName, result);
+      fs.writeFileSync(`${outputFileName}.csv`, result);
     })
     .catch(err => console.log('err!', err));
 }
 
 (async () => {
-  /*
-  const { data } = await getTransactions();
-  const { transactions } = data;
+  intro(`lunch-money-exporter`);
 
-  fs.writeFileSync(`${DEFAULT_NAME}.json`, JSON.stringify(transactions));
-  convertToCSV(transactions);
-  */
-
-  intro(`create-my-app`);
   const apiKey = await text({
-    message: 'Add your API key. You can get it on https://my.lunchmoney.app/developers!',
+    message: 'Insert your LunchMoney API key. You can get one on https://my.lunchmoney.app/developers.',
     validate(value) {
-      if (value.length === 0) return `Value is required!`;
+      if (value.length === 0) return `The API key is required!`;
+    },
+  });
+
+  const startDate = await text({
+    message: 'Export transactions from this (start) date...',
+    placeholder: 'YYYY-MM-DD',
+    validate(value) {
+      const regEx = /^\d{4}-\d{2}-\d{2}$/;
+      if (!value.match(regEx)) return 'Invalid date format!';
+    }
+  });
+
+  const endDate = await text({
+    message: '...until this (end) date.',
+    placeholder: 'YYYY-MM-DD',
+    initialValue: dayjs().format('YYYY-MM-DD'),
+    validate(value) {
+      const regEx = /^\d{4}-\d{2}-\d{2}$/;
+      if (!value.match(regEx)) return 'Invalid date format!';
+    }
+  });
+
+  const fileName = await text({
+    message: 'What should the file be named?',
+    initialValue: 'exported-transactions',
+    validate(value) {
+      if (value.length === 0) return `The file name is required!`;
     },
   });
 
@@ -71,14 +87,19 @@ function convertToCSV(input: Array<object> = [], outputFileName: string = `${DEF
     message: 'Do you want to save it on a JSON file too?',
   });
 
-  const { data } = await getTransactions(String(apiKey));
+  s.start('Getting transactions...');
+  const { data } = await getTransactions(String(apiKey), String(startDate), String(endDate));
   const { transactions } = data;
+  s.stop('Transactions downloaded!');
 
   if (saveJsonFile) {
-    fs.writeFileSync(`${DEFAULT_NAME}.json`, JSON.stringify(transactions));
+    fs.writeFileSync(`${String(fileName)}.json`, JSON.stringify(transactions));
   }
-  convertToCSV(transactions);
 
-  outro(`You're all set!`);
+  s.start('Converting to CSV...');
+  convertToCSV(transactions, String(fileName));
+  s.stop('Data converted!');
+
+  outro(`You're all set! File saved in ${String(fileName)}.csv`);
 })();
 
